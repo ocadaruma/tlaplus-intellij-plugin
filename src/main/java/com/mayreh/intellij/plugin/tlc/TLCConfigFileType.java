7c88231f3ed4d163b4fa8414ba276ab0d614d8fd
Haruki Okada <ocadaruma@gmail.com>
2021-11-12 08:07:49 +0900

    custom filetype detection

--- src/main/java/com/mayreh/intellij/plugin/tlc/TLCConfigFileType.java
+++ src/main/java/com/mayreh/intellij/plugin/tlc/TLCConfigFileType.java
@@ -2,8 +2,15 @@ package com.mayreh.intellij.plugin.tlc;
 
 import javax.swing.Icon;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.FileTypeRegistry.FileTypeDetector;
 import com.intellij.openapi.fileTypes.LanguageFileType;
+import com.intellij.openapi.util.io.ByteSequence;
+import com.intellij.openapi.vfs.VirtualFile;
 
 public class TLCConfigFileType extends LanguageFileType {
     public static final TLCConfigFileType INSTANCE = new TLCConfigFileType();
@@ -24,11 +31,33 @@ public class TLCConfigFileType extends LanguageFileType {
 
     @Override
     public String getDefaultExtension() {
-        return "cfg";
+        return "";
     }
 
     @Override
     public Icon getIcon() {
         return AllIcons.FileTypes.Any_type;
+    }
+
+    /**
+     * Since .cfg is a common extension widely used by other tools (e.g. Ansible),
+     * we resolve file type to TLC model checker config only when .tla file exists in
+     * same directory.
+     */
+    public static class Detector implements FileTypeDetector {
+        @Override
+        public @Nullable FileType detect(@NotNull VirtualFile file, @NotNull ByteSequence firstBytes,
+                                         @Nullable CharSequence firstCharsIfText) {
+            if (".cfg".equals(file.getExtension())) {
+                return null;
+            }
+            if (file.getParent() == null) {
+                return null;
+            }
+            if (file.getParent().findChild(file.getNameWithoutExtension() + ".tla") != null) {
+                return INSTANCE;
+            }
+            return null;
+        }
     }
 }
