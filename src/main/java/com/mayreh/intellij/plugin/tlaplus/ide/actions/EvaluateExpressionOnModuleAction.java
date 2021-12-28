1dbb0d1e3791c31ede1d53265631329cf384c4df
Haruki Okada <ocadaruma@gmail.com>
2021-12-28 20:58:55 +0900

    grouping actions

--- src/main/java/com/mayreh/intellij/plugin/tlaplus/ide/actions/EvaluateExpressionOnModuleAction.java
+++ src/main/java/com/mayreh/intellij/plugin/tlaplus/ide/actions/EvaluateExpressionOnModuleAction.java
@@ -1,16 +1,22 @@
 
 package com.mayreh.intellij.plugin.tlaplus.ide.actions;
 
+import java.nio.file.Path;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProviderBase;
 import com.intellij.xdebugger.impl.breakpoints.XExpressionImpl;
 import com.mayreh.intellij.plugin.tlaplus.TLAplusFile;
@@ -19,23 +25,28 @@ import com.mayreh.intellij.plugin.tlaplus.fragment.TLAplusFragmentFile;
 import com.mayreh.intellij.plugin.tlaplus.fragment.TLAplusFragmentFileType;
 import com.mayreh.intellij.plugin.tlaplus.fragment.TLAplusFragmentLanguage;
 import com.mayreh.intellij.plugin.tlaplus.psi.TLAplusModule;
+import com.mayreh.intellij.plugin.tlaplus.run.eval.Context;
 import com.mayreh.intellij.plugin.tlaplus.run.eval.DummyModule;
 import com.mayreh.intellij.plugin.tlaplus.run.eval.EvaluateExpressionDialog;
 import com.mayreh.intellij.plugin.util.TLAplusTreeUtil;
 
-public class EvaluateExpressionAction extends DumbAwareAction {
+public class EvaluateExpressionOnModuleAction extends TLAplusActionBase {
     @Override
-    public void update(@NotNull AnActionEvent e) {
-        super.update(e);
-        e.getPresentation().setEnabled(e.getProject() != null);
-    }
-
-    @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
-        if (e.getProject() == null) {
-            return;
+    protected void doAction(@NotNull AnActionEvent e,
+                            @NotNull Project project,
+                            @NotNull TLAplusDocument document) {
+        TLAplusModule currentModule = PsiTreeUtil.findChildOfType(document.file(), TLAplusModule.class);
+        final String currentModuleName;
+        if (currentModule != null) {
+            currentModuleName = currentModule.getModuleHeader().getName();
+        } else {
+            currentModuleName = null;
         }
-        new EvaluateExpressionDialog(e.getProject(), null, new XDebuggerEditorsProviderBase() {
+
+        Pair<Context, PsiDirectory> context = maybeContext(currentModuleName, document);
+        FileDocumentManager.getInstance().saveAllDocuments();
+
+        new EvaluateExpressionDialog(project, context == null ? null : context.first, new XDebuggerEditorsProviderBase() {
             @Override
             protected PsiFile createExpressionCodeFragment(
                     @NotNull Project project,
@@ -47,6 +58,9 @@ public class EvaluateExpressionAction extends DumbAwareAction {
                 moduleBuilder
                         .append("---- MODULE ").append(DummyModule.moduleName()).append(" ----").append('\n')
                         .append("EXTENDS Reals,Sequences,Bags,FiniteSets,TLC,Randomization");
+                if (currentModuleName != null) {
+                    moduleBuilder.append(',' + currentModuleName);
+                }
                 moduleBuilder.append('\n');
                 moduleBuilder.append("====");
 
@@ -63,7 +77,15 @@ public class EvaluateExpressionAction extends DumbAwareAction {
                                             TLAplusFragmentLanguage.INSTANCE,
                                             text);
 
+                // We can't set currentModule as the fragment's module directory because:
+                // - currentModule's local definitions (e.g. LOCAL operators) shouldn't be appeared in
+                //   completion candidates because they can't be referred when evaluating an expression (i.e.
+                //   it creates dummy module that extends currentModule, so only can refer public definitions).
+                //   We set dummy module here to resolve references in same logic as expression evaluation.
                 TLAplusTreeUtil.findChildOfType(dummyModuleFile, TLAplusModule.class).ifPresent(fragment::setModule);
+                if (context != null) {
+                    dummyModuleFile.setDirectory(context.second);
+                }
                 return fragment;
             }
 
@@ -72,5 +94,30 @@ public class EvaluateExpressionAction extends DumbAwareAction {
                 return TLAplusFragmentFileType.INSTANCE;
             }
         }, XExpressionImpl.EMPTY_EXPRESSION).show();
+    }
+
+    private static @Nullable Pair<Context, PsiDirectory> maybeContext(
+            @Nullable String moduleName,
+            TLAplusDocument document) {
+        if (moduleName == null) {
+            return null;
+        }
+
+        VirtualFile virtualFile = document.file().getVirtualFile();
+        if (virtualFile == null) {
+            return null;
+        }
+
+        PsiDirectory directory = document.file().getContainingDirectory();
+        if (directory == null) {
+            return null;
+        }
+
+        Path path = virtualFile.getFileSystem().getNioPath(directory.getVirtualFile());
+        if (path == null) {
+            return null;
+        }
+
+        return Pair.pair(new Context(moduleName, path), directory);
     }
 }
